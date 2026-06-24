2718618a92cbe7ebdf2aafede531acece9b6ca6b
Oscar Spruit <oscarspruit@gmail.com>
2026-06-24 21:56:37 +0200

    Add base annotation classes

---
+++ runtime/src/main/kotlin/dev/oscarspruit/unsealed/runtime/UnsealedLeaf.kt
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2026 Oscar Spruit
+ *
+ * This file is open source and available under the Apache 2.0 license. See the LICENSE file for more info.
+ */
+
+package dev.oscarspruit.unsealed.runtime
+
+@Target(AnnotationTarget.CLASS)
+annotation class UnsealedLeaf
