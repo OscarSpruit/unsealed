596c944d11b554abd8cab89428d3b529fef0b216
Oscar Spruit <oscarspruit@gmail.com>
2026-06-26 16:54:46 +0200

    Set up modules for compiler plugin

---
+++ compiler/build.gradle.kts
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2026 Oscar Spruit
+ *
+ * This file is open source and available under the Apache 2.0 license. See the LICENSE file for more info.
+ */
+
+plugins {
+    kotlin("jvm")
+}
+
+dependencies {
+    compileOnly(libs.kotlin.compiler.embeddable)
+}
